43acc503aadb972c8922d77038829f009dd9deb5
Sam <floatingdino@gmail.com>
2015-08-31 21:04:14 +1000

    basic layout
    created homepage, initial image assets, stylesheets and pdo file

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+    <!-- TEAMWORKER 1.0 -->
+
+    <!-- TODO: meta -->
+    <title>TeamWorker</title>
+
+    <!-- STYLESHEETS -->
+    <link href='https://fonts.googleapis.com/css?family=Lato:300,900' rel='stylesheet' type='text/css'>
+    <link href='style.css' rel='stylesheet' type='text/css'>
+    <link href='colour.css' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+
+    <main>
+        <?php include "header.inc"; ?>
+        <div class="content">
+            <h1>Curly Braces</h1>
+        </div>
+    </main>
+
+</body>
+</html>
