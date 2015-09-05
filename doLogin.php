07122058aa806224ed6b0dc203ebe8d3caf65a10
Ruslan Abylkassov <ruslan.a@me.com>
2015-09-05 15:07:05 +1000

    removed some debug code

--- doLogin.php
+++ doLogin.php
@@ -1,13 +1,9 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 include('pdo.inc');
 
 $email = $_POST['email'];
 //$password = hash('sha256', $_POST['password']);
 $password = $_POST['password'];
-
-echo "posted: ".$email." ".$password;
 
 $query = ('SELECT * FROM users WHERE email = :email');
 $statement = $db -> prepare($query);
@@ -24,6 +20,6 @@ if($dbPass == $password) {
 	// if you just registered, add yourself as a friend before showing the news feed
 	echo "<meta http-equiv='REFRESH' content='0;url=index.php?loggedin=true'>";
 } else {
-	echo "wrong";
+	echo "wrong login";
 }
 ?>
