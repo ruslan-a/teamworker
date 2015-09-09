d96d106bd0f82b665fbf3a2ee8fc930d3c188010
Sam <floatingdino@gmail.com>
2015-09-09 22:47:25 +1000

    styling on these fools
    updated styles for filters and login page

---
+++ filters.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+    <!-- TEAMWORKER 1.0 -->
+
+    <!-- TODO: meta -->
+    <title>TeamWorker</title>
+
+    <!-- STYLESHEETS -->
+    <link href='https://fonts.googleapis.com/css?family=Lato:300,900' rel='stylesheet' type='text/css'>
+    <link href='style.css' rel='stylesheet' type='text/css'>
+    <link href='colour.css' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+
+    <main>
+        <?php include "header.inc"; ?>
+        <div class="two">
+            <h1>Left Column</h1>
+            <h2>Three Column Filter List</h2>
+            <ul class="filters three">
+                <li><span>Filter One</span></li>
+                <li><span>Filter Two</span></li>
+                <li><span>Filter Three</span></li>
+            </ul>
+            <h2>Four Column Filter List</h2>
+            <ul class="filters four">
+                <li><span>1</span></li>
+                <li><span>2</span></li>
+                <li><span>3</span></li>
+                <li><span>4</span></li>
+            </ul>
+        </div>
+        <div class="two">
+            <h1>Right Column</h1>
+        </div>
+    </main>
+
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,14 +1,31 @@
-<!doctype html>
-<html lang="en">
 <head>
-	<title>Login</title>
+
+    <!-- TEAMWORKER 1.0 -->
+
+    <!-- TODO: meta -->
+    <title>TeamWorker</title>
+
+    <!-- STYLESHEETS -->
+    <link href='https://fonts.googleapis.com/css?family=Lato:300,900' rel='stylesheet' type='text/css'>
+    <link href='style.css' rel='stylesheet' type='text/css'>
+    <link href='colour.css' rel='stylesheet' type='text/css'>
+
 </head>
 <body>
-		<h2>Log in</h2>
-			<form id="mainForm" name="myForm" action="doLogin.php" method="post">
-				<input type="text" name="email" placeholder="email"><br>
-				<input type="password" name="password" placeholder="password"><br>
-				<input type="submit" value="Log in">
-			</form>
+
+    <main>
+        <?php include "header.inc"; ?>
+        <div class="content">
+			<h1>Log in</h1>
+				<form id="login" name="myForm" action="doLogin.php" method="post">
+					<label for="email">QUT email</label>
+					<label for="password">Password</label>
+					<input type="email" name="email" placeholder="email" id="email">
+					<input type="password" name="password" placeholder="password" id="password"><br>
+					<input type="submit" value="Log in">
+				</form>
+        </div>
+    </main>
+
 </body>
 </html>
