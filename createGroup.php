e2cdfbc7001f863f628d6496203589258af6100d
Sam <floatingdino@gmail.com>
2015-09-30 16:32:51 +1000

    various design updates
    conformed group creation page to main style, made tweaks to forms, esp.
    submit and textarea

--- createGroup.php
+++ createGroup.php
@@ -1,7 +1,7 @@
 <?php include('userHandler.php'); ?>
 <?php include('groupHandler.php'); ?>
 
-<?php 
+<?php
 
 if(isset($_GET['error']) && $_GET['error'] == 'db') {
   echo 'some sort of db error, try again maybe?';
@@ -12,19 +12,31 @@ if(isset($_GET['action']) && $_GET['action'] == "create") {
   createGroup($_POST['name'], $_POST['description'], $userId, $db);
 } else { ?>
 <!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
   <title>Create a Group - Teamworker</title>
+  <!-- STYLESHEETS -->
+  <link href='https://fonts.googleapis.com/css?family=Lato:300,900' rel='stylesheet' type='text/css'>
+  <link href='style.css' rel='stylesheet' type='text/css'>
+  <link href='colour.css' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
-  <h2>Create a Group</h2>
-  <form id="mainForm" name="regoForm" action="createGroup.php?action=create" method="post"><br>
-    <input type="text" name="name" placeholder="Group Name" /><br>
-    <input type="text" name="type" placeholder="Project Type" /><br>
-    <textarea name="description" placeholder="Project Description"></textarea><br>
-    <input type="submit" value="Create"/>
-  </form>
+    <main>
+    <?php include "header.inc"; ?>
+    <div class="content">
+      <h1>Create a Group</h1>
+      <form id="mainForm" name="regoForm" action="createGroup.php?action=create" method="post">
+        <label for="name">Name your Group</label><input type="text" name="name" id="name" placeholder="Group Name" required />
+        <label for="type">Enter Project Type</label><input type="text" name="type" id="type" placeholder="Project Type" />
+        <label for="grade">Grade Aim</label><input type="number" name="gradeWanted" id="grade" placeholder="4" min="4" max="7"/>
+        <label for="end">Projected Completion Date</label><input type="date" name="projectEnd" id="end" required/>
+        <label for="max">Maximum Number of Members</label><input type="number" name="memberMax" id="max" placeholder="3" min="3" max="6"/>
+        <textarea name="description" placeholder="Project Description"></textarea>
+        <input type="submit" value="Create" />
+      </form>
+    </div>
+  </main>
 </body>
 </html>
 <?php } ?>
